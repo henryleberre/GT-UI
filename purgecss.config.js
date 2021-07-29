40810e994f18d9ec8a134aa5df549a964a5ead6e
Henry LE BERRE <hleberre64@gmail.com>
2021-07-28 23:00:43 -0400

    Minor Fixes

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-    content: [
-        'src/content/gtui.html',
-        'src/content/main.js'
-    ],
-    css: [
-        'src/lib/tailwind.min.css'
-    ],
     defaultExtractor: content => content.match(/[\w-:/]+(?<!:)/g) || []
 }
