75c2ee042e0471a03dc0bc554fca8aede2a24475
Henry A. Le Berre <24208000+henryleberre@users.noreply.github.com>
2021-09-07 13:52:30 -0400

    build.py

--- build.py
+++ build.py
@@ -1,20 +1,31 @@
+#!/usr/bin/env python3
+
 import pathlib, os, json, shutil
-from PIL import Image
 
-def GetFilename(filepath):
-    return filepath.split("/")[-1]
+from tqdm import tqdm
+from PIL  import Image
 
-def CreateDirectory(name):
+def get_filename(filepath):
+    return os.path.basename(filepath)
+
+def create_directory(name):
     os.makedirs(name, exist_ok=True)
 
-def CopyFilesToDirectory(filenames, directory):
+def create_directories(names):
+    for name in names:
+        create_directory(name)
+
+def copy_files_to_directory(filenames, directory):
     for filename in filenames:
         shutil.copy(filename, directory)
 
-def GetPathsToAllFilesInDirectory(directory):
+def get_paths_to_files_in_directory(directory):
+    if not directory.endswith("/"):
+        directory = directory + "/"
+    
     return [ directory + filename for filename in os.listdir(directory) ]
 
-def FilterEndsWith(arr, arr_s):
+def filter_ends_with(arr, arr_s):
     def _predicate(filename):
         for s in arr_s:
             if filename.endswith(s):
@@ -24,7 +35,7 @@ def FilterEndsWith(arr, arr_s):
 
     return list(filter(_predicate, arr))
 
-def FilterNotEndsWith(arr, arr_s):
+def filter_not_end_with(arr, arr_s):
     def _predicate(filename):
         for s in arr_s:
             if filename.endswith(s):
@@ -34,155 +45,191 @@ def FilterNotEndsWith(arr, arr_s):
     
     return list(filter(_predicate, arr))
 
-def WithoutDuplicates(arr):
+def get_without_duplicates(arr):
     return list(set(arr))
 
-def PurgeCSS(dest, inCSS, inContent):
-    os.system("purgecss --config ./purgecss.config.js --output {} --css {} --content {}".format(dest, inCSS, inContent))
+def add_trailing_slash(s):
+    if s.endswith("/"):
+        return s
 
-def PurgeCSSMultiple(destFolder, inCSSFilepaths, inContent):
+    return s + "/"
+
+def purge_css(destFolder, inCSSFilepaths, inContent):
+    base = "purgecss --config ./purgecss.config.js --output {} --css {} --content {}"
     for filepath in inCSSFilepaths:
-        PurgeCSS(destFolder + "/" + GetFilename(filepath), filepath, inContent)
+        dstFilepath = add_trailing_slash(destFolder) + get_filename(filepath)
 
-# Clean
-shutil.rmtree("build/", ignore_errors=True)
+        os.system(base.format(dstFilepath, filepath, inContent))
 
-CONFIG = json.loads(open("build.config.json").read())
+def read_json_file(filename):
+    f = open(filename, "r")
+    r = json.loads(f.read())
+    f.close()
 
-print("Building {} @ v{}".format(CONFIG["name"], CONFIG["version"]))
-CreateDirectory("build")
+    return r
 
-print("- Common Stage:")
-CreateDirectory("build/common")
+def write_json_file(filename, data):
+    f = open(filename, "w")
+    json.dump(data, open(filename, "w"), indent=4)
+    f.close()
 
-MANIFEST_COMMON = {
-    "name":        CONFIG["name"],
-    "version":     CONFIG["version"],
-    "permissions": CONFIG["permissions"],
-    "icons": {},
-}
+def delete_directory(directory):
+    shutil.rmtree(directory, ignore_errors=True)
 
-print("  - Generating Icons...")
-CreateDirectory("build/common/icons")
+def has_key(obj, key):
+    return key in list(obj.keys())
 
-icon = Image.open(CONFIG["icon"]["filename"])
-for size in CONFIG["icon"]["sizes"]:
-    icon.resize((int(size), int(size))).save("build/common/icons/icon{}.png".format(size))
-    MANIFEST_COMMON["icons"][size] = "icons/icon{}.png".format(size)
+def get_with_key_or_fail(obj, key, err):
+    if has_key(obj, key):
+        return obj[key]
+    
+    print("[FATAL ERROR: {}]".format(err))
+    exit(-1)
 
-print("  - Generating Content Scripts...")
-for i in range(len(CONFIG["content_scripts"])):
-    cs = CONFIG["content_scripts"][i]
-    CreateDirectory("build/common/{}".format(cs["id"]))
+def get_with_key_or(obj, key, val):
+    if has_key(obj, key):
+        return obj[key]
+    
+    return val
 
-    cs_files = cs["loads"] + cs["accesses"]
-
-    cs_files_css     = FilterEndsWith(cs_files, [".css"])
-    cs_files_not_css = FilterNotEndsWith(cs_files, [".css"])
-
-    CopyFilesToDirectory(cs_files_not_css, "build/common/{}".format(cs["id"]))
-
-    PurgeCSSMultiple("build/common/{}".format(cs["id"]), cs_files_css, " ".join(cs_files_not_css))
-
-print("  - Generating Popup...")
-CreateDirectory("build/common/popup")
-
-popup_css_files     = FilterEndsWith(CONFIG["popup"], [".css"])
-popup_not_css_files = FilterNotEndsWith(CONFIG["popup"], [".css"])
-
-CopyFilesToDirectory(popup_not_css_files, "build/common/popup")
-PurgeCSSMultiple("build/common/popup", popup_css_files, " ".join(popup_not_css_files))
-
-print("- Firefox Stage:")
-CreateDirectory("build/Firefox")
-
-MANIFEST_V2 = {
-    "manifest_version": 2,
-    "content_scripts": [],
-    "web_accessible_resources": [],
-    "browser_action": {
+CONFIG           = {}
+MANIFEST_COMMON  = {}
+POPUP_OBJ_COMMON = {
         "default_icon": {},
-        "default_title": CONFIG["name"],
-        "default_popup": "popup/" + GetFilename(FilterEndsWith(CONFIG["popup"], [".html"])[0])
-    },
-    "browser_specific_settings": CONFIG["browser_specific_settings"]
+        "default_title": "",
+        "default_popup": ""
 }
 
-MANIFEST_V2.update(MANIFEST_COMMON)
+def build_common():
+    global MANIFEST_COMMON
+    global POPUP_OBJ_COMMON
 
-print("  - Copying Icons...")
-CreateDirectory("build/Firefox/icons")
-CopyFilesToDirectory([ "build/common/icons/icon{}.png".format(size) for size in CONFIG["icon"]["sizes"] ], "build/Firefox/icons")
+    create_directories(["build/Common", "build/Common/icons", "build/Common/popup"])
 
-print("  - Handling Content Scripts...")
-for i in range(len(CONFIG["content_scripts"])):
-    cs = CONFIG["content_scripts"][i]
+    MANIFEST_COMMON = {
+        "name":            get_with_key_or     (CONFIG, "name",    "build.config.json doesn't have a name field"),
+        "version":         get_with_key_or_fail(CONFIG, "version", "build.config.json doesn't have a version field"),
+        "permissions":     get_with_key_or     (CONFIG, "permissions", {}),
+        "icons":           {},
+        "content_scripts": []
+    }
 
-    CreateDirectory("build/Firefox/{}".format(cs["id"]))
+    # Generate Icons
+    icon = Image.open(CONFIG["icon"]["filename"])
+    for size in CONFIG["icon"]["sizes"]:
+        dims          = (size, size)
+        filename_icon = "build/Common/icons/icon{}.png".format(size)
+        icon.resize(dims).save(filename_icon)
 
-    filepaths = GetPathsToAllFilesInDirectory("build/common/{}/".format(cs["id"]))
-    CopyFilesToDirectory(filepaths, "build/Firefox/{}".format(cs["id"]))
+        MANIFEST_COMMON["icons"][str(size)] = "icons/icon{}.png".format(size)
 
-    csObj = {}
-    csObj["matches"] = cs["matches"]
+    # Generate Content Scripts
+    for cs in CONFIG["content_scripts"]:
+        create_directories(["build/Common/{}".format(cs["id"])])
 
-    for ext in ["css", "js"]:
-        csObj[ext] = FilterEndsWith([ "{}/".format(cs["id"]) + filename for filename in os.listdir("build/common/{}/".format(cs["id"])) ], [ext])
+        filenames = cs["loads"] + cs["accesses"]
 
-    MANIFEST_V2["content_scripts"].append(csObj)
+        filenames_css     = filter_ends_with   (filenames, [".css"])
+        filenames_not_css = filter_not_end_with(filenames, [".css"])
 
-    MANIFEST_V2["web_accessible_resources"] = WithoutDuplicates(MANIFEST_V2["web_accessible_resources"] + [ "{}/".format(cs["id"]) + GetFilename(filepath) for filepath in cs["accesses"] ])
+        copy_files_to_directory(filenames_not_css, "build/Common/{}".format(cs["id"]))
 
-print("  - Handling Popup...")
-CreateDirectory("build/Firefox/popup")
-CopyFilesToDirectory(GetPathsToAllFilesInDirectory("build/common/popup/"), "build/Firefox/popup")
+        purge_css("build/Common/{}".format(cs["id"]), filenames_css, " ".join(filenames_not_css))
 
-print("  - Adding Icons to Manifest..")
-for size in CONFIG["icon"]["sizes"]:
-    MANIFEST_V2["browser_action"]["default_icon"][size] = "icons/icon{}.png".format(size)
+        # Fill Manifest's content_scripts
+        obj = {}
+        obj["matches"] = cs["matches"]
+        
+        for ext in ["css", "js"]:
+            obj[ext] = filter_ends_with([ "{}/".format(cs["id"]) + filename for filename in os.listdir("build/Common/{}/".format(cs["id"])) ], [ext])
 
-print("  - Writing Manifest...")
-json.dump(MANIFEST_V2, open("build/Firefox/manifest.json", "w"), indent=4)
+        MANIFEST_COMMON["content_scripts"].append(obj)
 
-print("  - Creating Archive...")
-shutil.make_archive("build/Firefox", "zip", "build/Firefox/")
+    # Handle Popup
+    popup_css_filenames     = filter_ends_with   (CONFIG["popup"], [".css"])
+    popup_not_css_filenames = filter_not_end_with(CONFIG["popup"], [".css"])
 
-print("- Chromium Stage:")
-CreateDirectory("build/Chromium")
+    copy_files_to_directory(popup_not_css_filenames, "build/Common/popup")
+    purge_css("build/Common/popup", popup_css_filenames, " ".join(popup_not_css_filenames))
 
-print("  - Copying Common & Firefox...")
-CreateDirectory("build/Chromium/icons")
-CopyFilesToDirectory(GetPathsToAllFilesInDirectory("build/common/icons/"), "build/Chromium/icons")
+    # Create POPUP_OBJ_COMMON
+    POPUP_OBJ_COMMON["default_title"] = CONFIG["name"]
+    POPUP_OBJ_COMMON["default_popup"] = "popup/" + get_filename(filter_ends_with(CONFIG["popup"], [".html"])[0])
 
-CreateDirectory("build/Chromium/popup")
-CopyFilesToDirectory(GetPathsToAllFilesInDirectory("build/common/popup/"), "build/Chromium/popup")
+def build_for_browser(browser_obj):
+    global CONFIG
+    global MANIFEST_COMMON
 
-for i in range(len(CONFIG["content_scripts"])):
-    cs = CONFIG["content_scripts"][i]
+    browser_name     = get_with_key_or_fail(browser_obj, "name", "No browser name provided")
+    browser_manifest = get_with_key_or_fail(browser_obj, "manifest_version", "No manifest version provided")
 
-    CreateDirectory("build/Chromium/{}".format(cs["id"]))
-    CopyFilesToDirectory(GetPathsToAllFilesInDirectory("build/common/{}/".format(cs["id"])), "build/Chromium/{}".format(cs["id"]))
+    # Create Dirs
+    build_dir = "build/{}".format(browser_name) + "/"
 
-MANIFEST_V3 = {
-    "manifest_version": 3,
-    "web_accessible_resources": [],
-    "action": MANIFEST_V2["browser_action"],
-    "content_scripts": MANIFEST_V2["content_scripts"]
-}
+    create_directories([build_dir + "icons", build_dir + "popup"])
 
-MANIFEST_V3.update(MANIFEST_COMMON)
+    copy_files_to_directory(get_paths_to_files_in_directory("build/Common/icons"), build_dir + "icons")
+    copy_files_to_directory(get_paths_to_files_in_directory("build/Common/popup"), build_dir + "popup")
 
-print("  - Resolving Web Accessible Resources...")
-for i in range(len(CONFIG["content_scripts"])):
-    cs = CONFIG["content_scripts"][i]
+    for cs in CONFIG["content_scripts"]:
+        create_directories([build_dir + cs["id"]])
 
-    MANIFEST_V3["web_accessible_resources"].append({
-        "matches":   cs["matches"],
-        "resources": [ "{}/".format(cs["id"]) + GetFilename(filepath) for filepath in cs["accesses"] ]
-    })
+        copy_files_to_directory(get_paths_to_files_in_directory("build/Common/{}".format(cs["id"])), build_dir + cs["id"])
 
-print("  - Writing Manifest...")
-json.dump(MANIFEST_V3, open("build/Chromium/manifest.json", "w"), indent=4)
+    MANIFEST = {
+        "manifest_version": browser_manifest,
+        "web_accessible_resources": []
+    }
 
-print("  - Creating Archive...")
-shutil.make_archive("build/Chromium", "zip", "build/Chromium/")
+    MANIFEST.update(MANIFEST_COMMON)
+
+    if browser_manifest == 2:
+        for cs in CONFIG["content_scripts"]:
+            MANIFEST["web_accessible_resources"] += [ "{}/".format(cs["id"]) + get_filename(filepath) for filepath in cs["accesses"] ]
+            MANIFEST["web_accessible_resources"] = get_without_duplicates(MANIFEST["web_accessible_resources"])
+
+        if has_key(browser_obj["params"], "browser_specific_settings"):
+            MANIFEST["browser_specific_settings"] = browser_obj["params"]["browser_specific_settings"]
+
+        MANIFEST["browser_action"] = POPUP_OBJ_COMMON
+
+        if browser_obj["self_host"]:
+            MANIFEST["browser_specific_settings"] = {
+                "gecko": {
+                    "id": browser_obj["extension_id"],
+                    "update_url": browser_obj["params"]["updates.json_url"]
+                }
+            }
+    elif browser_manifest == 3:
+        for cs in CONFIG["content_scripts"]:
+            MANIFEST["web_accessible_resources"].append({
+                "matches":   cs["matches"],
+                "resources": [ "{}/".format(cs["id"]) + get_filename(filepath) for filepath in cs["accesses"] ]
+            })
+        
+        MANIFEST["action"] = POPUP_OBJ_COMMON
+    else:
+        print("Unknown Manifest Version")
+        exit(-1)
+
+    write_json_file(build_dir + "manifest.json", MANIFEST)
+
+    # Create Zip Archive
+    shutil.make_archive("build/{}".format(browser_obj["name"]), "zip", "build/{}/".format(browser_obj["name"]))
+
+def run():
+    global CONFIG
+
+    create_directories(["build"])
+
+    CONFIG = read_json_file("build.config.json")
+
+    print("Building {} @ v{}".format(CONFIG["name"], CONFIG["version"]))
+
+    build_common()
+
+    for browser in tqdm(get_with_key_or(CONFIG, "browsers", [])):
+        build_for_browser(browser)
+
+if __name__ == "__main__":
+    run()
